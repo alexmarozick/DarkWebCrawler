f955f0a60db5dbce55fe54646e4f008d0869205f
Seth Tal <seth.tal255@gmail.com>
2021-01-16 13:52:31 -0800

    Simple command line web crawler based on Abot example
    * taken from the Abot repo on github, I have implemented and partially documented a simple program that crawls starting from a given url.
    
    * Crawler program also attempts a Single Page Request from the same url
    
    * to run open command prompt or powershell, navigate inside of the "webcrawler" folder and type: dotnet run
    (you can also provide a custom url as an arguement to "run" otherwise it will crawl google.com)

--- webcrawler/Crawler.cs
+++ webcrawler/Crawler.cs
@@ -1,13 +1,90 @@
 using System;
-using System.Collections;
+using System.Threading.Tasks;
+using Abot2.Core;
+using Abot2.Crawler;
+using Abot2.Poco;
+using Serilog;
+
 
 namespace ScrapeAndCrawl
 {
+#region Crawler Class
+
+    /// <summary>
+    /// Simple webcrawler demo as shown in Abot repo. Run using: dotnet run "urlToCrawlFrom".
+    /// By default it will crawl the Google.com homepage.
+    /// </summary>
     class Crawler
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");
+            // "Log" from Serilog namespace
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            Log.Logger.Information("Minimal Crawler Demo start...");
+
+            // If a url is not provided then use default url
+            if (args.Length == 0)
+            {
+                await SimpleCrawler();
+                await SinglePageRequest();
+            }
+            else // use provided url passed in as an arg
+            {
+                await SimpleCrawler(args[0]);
+                await SinglePageRequest(args[0]);
+            }
+        }
+
+        /// <summary>
+        /// Method that crawls starting from the passed in url
+        /// </summary>
+        /// <param name="uriToCrawl"> String representing the url to start crawling from </param>
+        private static async Task SimpleCrawler(string uriToCrawl = "http://google.com")
+        {
+            // "CrawlConfiguration" from Abot2.Poco namespace
+            // For specific configuration requirements the use of a "CrawlConfiguration" object
+            // can be used when creating crawlers like the one bellow...
+            var config = new CrawlConfiguration
+            {
+                MaxPagesToCrawl = 10, //Only crawl 10 pages
+                MinCrawlDelayPerDomainMilliSeconds = 3000 //Wait this many millisecs between requests
+            };
+
+            // "PoliteWebCrawler" from Abot2.Crawler namespace
+            // This creates a new "PoliteWebCrawler" object with configuration defined above
+            var crawler = new PoliteWebCrawler(config);
+
+            // Subscribes method "PageCrawlCompleted" to the PageCrawlCompleted event
+            crawler.PageCrawlCompleted += PageCrawlCompleted;
+
+            // Change the URL inside of the Uri object to have this crawler crawl somewhere else
+            var crawlResult = await crawler.CrawlAsync(new Uri(uriToCrawl));
+        }
+        
+        /// <summary>
+        /// An asyncronous single page request to ping a single page from the uriToRequest arg
+        /// </summary>
+        private static async Task SinglePageRequest(string uriToRequest = "https://google.com")
+        {
+            var pageRequester = new PageRequester(new CrawlConfiguration(), new WebContentExtractor());
+
+            var crawledPage = await pageRequester.MakeRequestAsync(new Uri(uriToRequest));
+            Log.Logger.Information("{result}", new
+            {
+                url = crawledPage.Uri,
+                status = Convert.ToInt32(crawledPage.HttpResponseMessage.StatusCode)
+            });
+        }
+
+        private static void PageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            var httpStatus = e.CrawledPage.HttpResponseMessage.StatusCode;
+            var rawPageText = e.CrawledPage.Content.Text;
         }
     }
+#endregion
 }
