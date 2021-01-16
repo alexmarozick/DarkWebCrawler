6f06f86aa19e2208839facc9c6fef1078085c4c5
Seth Tal <seth.tal255@gmail.com>
2021-01-16 12:53:29 -0800

    Basic .NET console app environment setup
    * Created basic .NET console app files, main program file is Crawler.cs

---
+++ webcrawler/Crawler.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+
+namespace ScrapeAndCrawl
+{
+    class Crawler
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
